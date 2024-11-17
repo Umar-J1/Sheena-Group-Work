4a873212dfc620818f8d79d756eb33ee59fe852b
Umar-J1 <124900819+Umar-J1@users.noreply.github.com>
2024-11-17 14:30:49 +0000

    just need to make it work

---
+++ template.php
@@ -0,0 +1,32 @@
+<?php
+    //
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php
+            require("includes/head.php");
+        ?>
+    </head>
+    <body>
+        <header>
+            <?php
+                include("includes/header.php")
+            ?>
+        </header>
+        <nav> 
+            <?php
+                include("includes/nav.php")
+            ?>
+        </nav>
+        <main>
+            
+        </main>
+        <footer>
+            <?php
+                include("includes/footer.php")
+            ?>
+        </footer>
+    </body>
+</html>
